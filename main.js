8eaeff179c324586da7e4808153c6b0db39f601d
Danhalf <danhel@rocketmail.com>
2023-07-14 07:53:34 +0300

    init: chat

--- main.js
+++ main.js
@@ -2,10 +2,26 @@ import { io } from 'https://cdn.socket.io/4.4.1/socket.io.esm.min.js';
 
 const socket = io('http://localhost:3000');
 
+const btn = document.getElementById('btn');
+const input = document.getElementById('input');
+const chat = document.querySelector('.chat');
+
 btn.addEventListener('click', (e) => {
-   socket.emit('message', 'ss')
+   e.preventDefault();
+   const { value } = input
+   socket.emit('message', value);
 });
-socket.on('connection', (arg) => {
-   console.log('arg: ', arg);
-   document.body.innerHTML += arg;
+
+socket.on('date', (date) => {
+   console.log('date: ', date);
 });
+
+socket.on('message', (msg) => {
+   const message = document.createElement('div');
+   message.classList.add('message');
+   message.textContent = msg;
+   chat.appendChild(message);
+   chat.scrollTop = chat.scrollHeight;
+   input.value = '';
+});
+
--- server.js
+++ server.js
@@ -6,10 +6,42 @@ const io = new Server(httpServer, {
    // options
 });
 
-io.on("connection", (socket) => {
-   // ...
-   console.log('connect on: ', socket.id)
-   socket.emit('<h2>Hello</h2>')
+io.on("connect", (socket) => {
+
+   socket.on("error", (err) => {
+      console.log('connection error: ', err)
+   })
+
+   socket.on("close", () => {
+      console.log('connection closed')
+   })
+   socket.on("disconnect", () => {
+      console.log('connection disconnected ', socket.id)
+   })
+   socket.on("reconnect", () => {
+      console.log('connection reconnected')
+   })
+   socket.on("reconnecting", () => {
+      console.log('connection reconnected')
+   })
+   socket.on("reconnect_failed", () => {
+      console.log('connection reconnected')
+   })
+   socket.on("reconnect_error", () => {
+      console.log('connection reconnected')
+   })
+
+
+   socket.on('message', (message) => {
+      console.log(`message via: ${socket.id}, ${message}`)
+      socket.emit('message', `${socket.id}: ${message}`)
+   })
+
+   socket.emit('date', new Date().toDateString());
+
 });
+
+
+
 
 httpServer.listen(3000);
